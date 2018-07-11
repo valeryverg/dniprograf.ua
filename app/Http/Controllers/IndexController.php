530b84e79332b071cd199ef7a39df925d7f9eea5
Valery <valeryverg@gmail.com>
2018-07-11 14:21:59 +0300

    COMMIT

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Menu as BDMenu;
-use Lavary\Menu\Menu;
+//use Lavary\Menu\Menu;
 
 class IndexController extends Controller
 {
@@ -17,20 +17,20 @@ class IndexController extends Controller
     {
         $menu = BDMenu::all()->sortBy('id');
 
-        $menuBuilder = new Menu();
-        $menuBuilder->make('NavBar', function ($m) use ($menu)
-        {
-            foreach ($menu as $item) {
-                if ($item->parent_id == 0) {
-                    $m->add($item->name, $item->link, $item->id_role)->id($item->id);
-                }
-                else {
-                    if ($m->find($item->parent_id)) {
-                        $m->find($item->parent_id)->add($item->name, $item->link, $item->id_role)->id($item->id);
-                    }
-                }
-            }
-        });
+//        $menuBuilder = new Menu();
+//        $menuBuilder->make('NavBar', function ($m) use ($menu)
+//        {
+//            foreach ($menu as $item) {
+//                if ($item->parent_id == 0) {
+//                    $m->add($item->name, $item->link, $item->id_role)->id($item->id);
+//                }
+//                else {
+//                    if ($m->find($item->parent_id)) {
+//                        $m->find($item->parent_id)->add($item->name, $item->link, $item->id_role)->id($item->id);
+//                    }
+//                }
+//            }
+//        });
 
         //dump($menuBuilder);
 

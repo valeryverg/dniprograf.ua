050eda7dfff954e66f7720ebb9447f0c4818e45e
valeryverg <38358050+valeryverg@users.noreply.github.com>
2018-07-11 18:34:55 +0300

    commit

--- database/migrations/2018_07_10_000004_create_table_submenus.php
+++ database/migrations/2018_07_10_000004_create_table_submenus.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableMenus extends Migration
+class CreateTableSubMenus extends Migration
 {
     /**
      * Run the migrations.

9f4891b991fbea542af60352f26392fc9be290b1
valeryverg <38358050+valeryverg@users.noreply.github.com>
2018-07-12 19:01:50 +0300

    commit

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -9,97 +9,40 @@
                             src="{{ asset('images/logo/logo_dniprograf.png') }}"
                             alt="Дніпрограф" height="50" width="160"/></a>
             </div>
+
             <div class="nav-fill"></div>
 
             {{--<div class="container">--}}
-                {{--<nav class="navbar navbar-default" role="navigation">--}}
-                    <ul class="nav navbar-nav navbar-left">
-                        @foreach($menu as $item)
-                            @if($item->submenu->count() > 0)
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                        {{ $item->name }}
-                                        <b class="caret"></b></a>
-                                    <ul class="dropdown-menu">
-                                        @foreach($item->submenu as $subitem)
-                                            <li><a href="{{ $item->link .'/'. $subitem->sublink }}">{{ $subitem->name }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                            @else
-                                <li><a href="{{ $item->link }}">{{ $item->name }}</a></li>
-                            @endif
-                        @endforeach
-                    </ul>
-                {{--</nav>--}}
+            {{--<nav class="navbar navbar-default" role="navigation">--}}
+            <ul class="nav navbar-nav navbar-left">
+                @foreach($menu as $item)
+                    @if($item->submenu->count() > 0)
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                {{ $item->name }}
+                                <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                @foreach($item->submenu as $subitem)
+                                    <li><a href="{{ $item->link .'/'. $subitem->sublink }}">{{ $subitem->name }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @else
+                        <li><a href="{{ $item->link }}">{{ $item->name }}</a></li>
+                    @endif
+                @endforeach
+            </ul>
+            {{--</nav>--}}
             {{--</div>--}}
 
-            {{-- <nav class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Введіть слово">
+                </div>
+                <button type="submit" class="btn btn-default">Пошук...</button>
+            </form>
 
-                 @if (isset($menu))
-                     <nav class="nav navbar-nav">
-                         @foreach ($menu as $item)
-                             @if ($item->submenu->count())
-                                 <div class="dropdown">
-                                     <button class="btn btn-secondary dropdown-toggle" type="button"
-                                             id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                                             aria-expanded="false">
-                                         {{ $item->name }}
-                                     </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         @foreach ($item->submenu as $subitem)
-                                             <a class="dropdown-item"
-                                                href="{{ $item->link .'/'. $subitem->sublink }}">{{ $subitem->name }}</a>
-                                         @endforeach
-                                     </div>
-                                 </div>
-                             @else
-                                 <a href="{{ $item->link }}">{{ $item->name }}</a>
-                             @endif
-                         @endforeach
-                     </nav>
-                 @endif--}}
-
-                {{--                @if (isset($menu))
-                                    <nav class="menu">
-                                        <ul>
-                                            @foreach ($menu as $item)
-                                                @if ($item->submenu->count())
-                                                    <li class="option">
-                                                        <div class="submenu">
-                                                            <a data-toggle="dropdown" href="#">
-                                                                <span>{{ $item->name }}</span>
-                                                                <b class="caret"></b>
-                                                            </a>
-                                                            <ul>
-                                                                @foreach ($item->submenu as $subitem)
-                                                                    <li>
-                                                                        <a href="{{ $item->link .'/'. $subitem->sublink }}">{{ $subitem->name }}</a>
-                                                                    </li>
-                                                                @endforeach
-                                                            </ul>
-                                                        </div>
-                                                    </li>
-                                                @else
-                                                    <li class="option">
-                                                        <a href="{{ $item->link }}">{{ $item->name }}</a>
-                                                    </li>
-                                                @endif
-                                            @endforeach
-                                        </ul>
-                                    </nav>
-                                @endif--}}
-
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Введіть слово">
-                    </div>
-                    <button type="submit" class="btn btn-default">Пошук...</button>
-                </form>
-
-            </nav>
-            {{--</div>--}}
-            {{--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">--}}
         </div><!-- /.container-fluid -->
     </nav>
 @endsection
--- resources/views/layout/sliderCSS.blade.php
+++ resources/views/layout/sliderCSS.blade.php
@@ -1,58 +1,131 @@
 @section('slider')
-    <link href="{{ asset ('css/sliderCSS.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset ('css/sliderCSS'. env('IMG_SIZE') .'.css') }}" rel="stylesheet" type="text/css"/>
     <script src="http://thecodeplayer.com/uploads/js/prefixfree.js" type="text/javascript"></script>
 
     <div class="container">
         <div class="container-fluid">
             <div class="slider">
-                <input type="radio" name="slide_switch" id="id1"/>
+                <!--Lets show the first image by default on page load-->
+                <input type="radio" name="slide_switch" id="id1" checked="checked"/>
                 <label for="id1">
-                    <img src="{{ asset('/images/slider/01.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/01.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/01.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/01.jpg') }}"/>
 
-                <!--Lets show the second image by default on page load-->
-                <input type="radio" name="slide_switch" id="id2" checked="checked"/>
+                <input type="radio" name="slide_switch" id="id2"/>
                 <label for="id2">
-                    <img src="{{ asset('/images/slider/02.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/02.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/02.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/02.jpg') }}"/>
 
                 <input type="radio" name="slide_switch" id="id3"/>
                 <label for="id3">
-                    <img src="{{ asset('/images/slider/03.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/03.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/03.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/03.jpg') }}"/>
 
                 <input type="radio" name="slide_switch" id="id4"/>
                 <label for="id4">
-                    <img src="{{ asset('/images/slider/04.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/04.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/04.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/04.jpg') }}"/>
 
-                <input type="radio" name="slide_switch" id="id5" width="480" height="360"/>
+                <input type="radio" name="slide_switch" id="id5"/>
                 <label for="id5">
-                    <img src="{{ asset('/images/slider/05.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/05.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/05.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/05.jpg') }}"/>
 
-                <input type="radio" name="slide_switch" id="id6"/>
+                <!--Lets show the image by default on page load-->
+                <input type="radio" name="slide_switch" id="id6" checked="checked"/>
                 <label for="id6">
-                    <img src="{{ asset('/images/slider/06.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/06.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/06.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/06.jpg') }}"/>
 
                 <input type="radio" name="slide_switch" id="id7"/>
                 <label for="id7">
-                    <img src="{{ asset('/images/slider/07.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/07.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/07.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/07.jpg') }}"/>
 
                 <input type="radio" name="slide_switch" id="id8"/>
                 <label for="id8">
-                    <img src="{{ asset('/images/slider/08.jpg') }}" width="60"/>
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/08.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
                 </label>
-                <img src="{{ asset('/images/slider/08.jpg') }}"/>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/08.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id9"/>
+                <label for="id9">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/09.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/09.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id10"/>
+                <label for="id10">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/10.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/10.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id11"/>
+                <label for="id11">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/11.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/11.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id12"/>
+                <label for="id12">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/12.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/12.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id13"/>
+                <label for="id13">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/13.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/13.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id14"/>
+                <label for="id14">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/14.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/14.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id15"/>
+                <label for="id15">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/15.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/15.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id16"/>
+                <label for="id16">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/16.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/16.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id17"/>
+                <label for="id17">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/17.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/17.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id18"/>
+                <label for="id18">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/18.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/18.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id19"/>
+                <label for="id19">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/19.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/19.jpg') }}"/>
+
+                <input type="radio" name="slide_switch" id="id20"/>
+                <label for="id20">
+                    <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/20.jpg') }}" width="{{ env('IMG_SMALL') }}"/>
+                </label>
+                <img src="{{ asset('/images/sliderCSS/'. env('IMG_SIZE') .'/20.jpg') }}"/>
             </div>
         </div>
     </div>
